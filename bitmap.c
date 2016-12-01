2610b5f56e23c89b74b37345ba504348ebaf52f7
dylanap <astain_d@epita.fr>
2016-12-02 02:10:21 +1100

    ADD #7 minor changes in rotate

--- bitmap.c
+++ bitmap.c
@@ -346,8 +346,17 @@ void rotate(bitmap *img, double angle)
   {
     x = i % img->width;
     y = (i - x) / img->width;
-    x2 = cos(angle) * (double)x + sin(angle) * (double)y;
-    y2 = - sin(angle) * (double)x + cos(angle) * (double)y;
+    //x2 = cos(angle) * (double)x + sin(angle) * (double)y;
+    //y2 = - sin(angle) * (double)x + cos(angle) * (double)y;
+    
+    x2 = x - tan(angle / 2) * y;
+    y2 = y;
+
+    x2 = x2;
+    y2 = y2 + sin(angle) * x2;
+
+    x2 = x2 - tan(angle / 2) * y2;
+    y2 = y2;
     if (x2 >= 0 && x2 < (double)img->width)
     {
       if (y2 >= 0 && y2 < (double)img->height)
