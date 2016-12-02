c77cfefd35243069fcb9eacc44d2e877fe76e6c3
MrColorado <theoissarni@hotmail.fr>
2016-12-02 12:09:18 +0100

    ADD #3 detection is finish there is no leak and doc is done

--- detection.h
+++ detection.h
@@ -1,3 +1,9 @@
+/**
+ * \file detection.h 
+ * \brief Detect text and only it in a image
+ * \author issarn_t
+ * \date 09/17/2016
+ */
 # ifndef DETECTION_H_DEFINED
 # define DETECTION_H_DEFINED
 # include <stdio.h>
@@ -5,32 +11,13 @@
 # include "bitmap.h"
 # include "queue.h"
 
-bitmap *binerizeCopy(bitmap *);
-
-void putLineMarker(bitmap *, char *, int, unsigned);
-
-void putColumnMarker(bitmap *, unsigned, unsigned, char *, int, unsigned);
-
-bitmap *cutBmp(bitmap *, unsigned, unsigned, unsigned, unsigned);
-
-float letterAverage(char *, unsigned);
-
-void segmentation(bitmap *, size_t *, size_t *, queue *, bitmap *, int);
-
-bitmap *widthTravel(bitmap *);
-
-bitmap *heightTravel(bitmap *);
-
-bitmap *merge(bitmap *, bitmap *);
-
-void makeHistogram(bitmap *, bitmap *, unsigned, unsigned, queue *);
-
-void textToHisto(queue *, bitmap *, bitmap *, float *, unsigned);
-
-void histoToImage(bitmap *, bitmap *, queue *, float *);
-
-bitmap *rlsa(bitmap *, queue *, queue *);
-
-queue *doTheThings(bitmap *, size_t *, size_t *);
+/**
+ * \brief return a queue which contain all letter 
+ *
+ * \param src is the original image
+ * \param nbCharacter is the number of character
+ * \param nbLetter is the number of letter
+ */
+queue *detectText(bitmap *, size_t *, size_t *);
 
 # endif
